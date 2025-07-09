5260c7e6020a30060d035a6c65f1584ee5516e14
Ethan <ethanlu941028@gmail.com>
2025-07-09 19:50:35 +0800

    weight with difficulty and proficiency

--- gui_main.py
+++ gui_main.py
@@ -40,11 +40,49 @@ def show_difficulty_button():
         #<Button-1> is left mouse click
         star_button.bind("<Button-1>", on_click)
     
+def show_proficiency_button():
+    
+    def set_proficiency(rating):
+        #store into the data
+        data[index]["proficiency"] = rating
         
+        #then write into the data.json
+        with open("data.json", "w") as f:
+            json.dump(data, f, indent = 4)
+            
+        #clear the star
+        for widget in proficiency_frame.winfo_children():
+            widget.destroy()
+    
+    #clean the old star
+    for widget in proficiency_frame.winfo_children():
+        widget.destroy()
+        
+    #build the star button
+    for i in range(1, 6):
+        #use canvas since in macOS, Button is not good                                      no frame(for look)
+        question_button = tk.Canvas(proficiency_frame, width = 60, height = 60, bg = "grey21", highlightthickness = 0)
+        question_button.pack(side = "left", padx = 3)
+        
+        question_button.create_text(30, 30, text = "?", font = ("Helvetica", 40), fill = "red")
+        
+        def on_click(event, rating = i):
+            set_proficiency(rating)
+            
+        #<Button-1> is left mouse click
+        question_button.bind("<Button-1>", on_click)
 
 def next_word():
     global index
-    index = random.randint(0, len(data) - 1)        
+    global weight_list
+    weight_list = []
+    for i in range(len(data)):
+        diff = data[i]["difficulty"] if data[i].get("difficulty") is not None else 3        #if the difficulty is None, the take 3
+        pro = data[i]["proficiency"] if data[i].get("proficiency") is not None else 3
+        for j in range(diff * (pro + 2)):
+            weight_list.append(i)
+        
+    index = random.choice(weight_list)      
     word_label.config(text = data[index]["word"], font = ("Helvetica", 28), fg = "black", bg = "white")  #change the text of the word label
     meaning_label.config(text = "", font = ("Helvetica", 28), fg = "black", bg = "grey21")  #clear the meaning label
     example_label.config(text = "", font = ("Helvetica", 28), fg = "black", bg = "grey21")  #clear the example label
@@ -57,6 +95,8 @@ def next_word():
         for widget in difficulty_frame.winfo_children():
             widget.destroy()
     
+    show_proficiency_button()
+        
 def add_word():
     
     global data
@@ -82,7 +122,9 @@ def add_word():
         new_entry = {
             "word" : word_entry.get(),
             "meaning" :  meaning_entry.get(),
-            "example" : example_entry.get()
+            "example" : example_entry.get(),
+            "difficulty" : None,
+            "proficiency" : None
         }
         
         data.append(new_entry)
@@ -102,7 +144,16 @@ with open("data.json", "r") as f:
         data = json.load(f)
     except json.JSONDecodeError:
         data = []
-index = random.randint(0, len(data) - 1)
+        
+#take the weight of the difficulty to decide which probaly will be the next word
+weight_list = []
+for i in range(len(data)):
+    diff = data[i]["difficulty"] if data[i].get("difficulty") is not None else 3        #if the difficulty is None, the take 3
+    pro = data[i]["proficiency"] if data[i].get("proficiency") is not None else 3
+    for j in range(diff * (pro + 2)):
+      weight_list.append(i)
+      
+index = random.choice(weight_list)
 
 window = tk.Tk()                #build the window object
 window.title("MyWordCoach")     #the window title
@@ -112,45 +163,53 @@ window.configure(bg = "grey21")
 
 #add Label to show the word
 word_label = tk.Label(window, text = data[index]["word"], font = ("Helvetica", 28), fg = "black", bg = "white")  #create a label with the word
-word_label.pack(pady = 20)      #add the label to the window and set the padding
+word_label.place(relx = 0.5, y = 50, anchor = "center")      #relative x(0 ~ 1)  anchor is the object's point
 
 #add the meaning label, and should be empty first
 meaning_label = tk.Label(window, font = ("Helvetica", 28), fg = "black", bg = "grey21")  #create a nothing label for the meaning first
-meaning_label.pack(pady = 30)   
+meaning_label.place(relx = 0.5, y = 150, anchor = "center")
 
 #add the example label
 example_label = tk.Label(window, font = ("Helvetica", 28), fg = "black", bg = "grey21")  #create a nothing label for the example first
-example_label.pack(pady = 30)
-
+example_label.place(relx = 0.5, y = 250, anchor = "center")
 
 
 
 #add a frame for the difficulty button
 difficulty_frame = tk.Frame(window, bg = "grey21")
-difficulty_frame.pack(pady = 20)
+difficulty_frame.place(relx = 0.5, y = 400, anchor = "center")
+
+#add a frame for proficiency button
+proficiency_frame = tk.Frame(window, bg = "grey21")
+proficiency_frame.place(relx = 0.5, y = 450, anchor = "center")
 
 
-#create a frame for the buttons
-button_frame = tk.Frame(window)
-button_frame.pack(pady = (100, 10))
+#frame for the buttons
+show_button_frame = tk.Frame(window, bg = "grey21")
+show_button_frame.place(relx = 0.5, y = 500, anchor = "center")
 
 #add the show_meaning button
-show_meaning_buttin = tk.Button(button_frame, text = "Show Meaning", command = show_meaning, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to show the meaning
+show_meaning_buttin = tk.Button(show_button_frame, text = "Show Meaning", command = show_meaning, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to show the meaning
 show_meaning_buttin.pack(side = "left")  
 
 #add the meaning label, and should be empty first
-show_example_buttin = tk.Button(button_frame, text = "Show Example", command = show_example, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to show the meaning
+show_example_buttin = tk.Button(show_button_frame, text = "Show Example", command = show_example, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to show the meaning
 show_example_buttin.pack(side = "right")
 
+#frame for next_word and add_word
+word_button_frame = tk.Frame(window, bg = "grey21")
+word_button_frame.place(relx = 0.5, y = 550, anchor = "center")
 
 #add next_word button
-next_word_button = tk.Button(window, text = "Next Word", command = next_word, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to show the next word
-next_word_button.pack()       
+next_word_button = tk.Button(word_button_frame, text = "Next Word", command = next_word, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to show the next word
+next_word_button.pack(side = "left") 
 
-add_word_button = tk.Button(window, text = "Add Word", command = add_word, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to add a new word
-add_word_button.pack()
+add_word_button = tk.Button(word_button_frame, text = "Add Word", command = add_word, font = ("Helvetica", 20), fg = "black", bg = "white")  #create a button to add a new word
+add_word_button.pack(side = "right") 
 
 if not data[index].get("difficulty"):
     show_difficulty_button()
+
+show_proficiency_button()
 
 window.mainloop()               #keep the window appearing
